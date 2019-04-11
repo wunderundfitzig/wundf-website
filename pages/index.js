993212dcb8429ab82f99a30ee50d0b424bd9dae8
Manuel Reich <mr@wundf.net>
2019-04-11 14:38:16 +0200

    Create Hello World with zeit and emotion

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Head from 'next/head'
+import styled from '@emotion/styled'
+
+const Frame = styled.div`
+  color: red;
+`
+
+export default () =>
+  <Frame>Hello World</Frame>
