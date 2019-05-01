ee9facaf27d909ea0200cee79611551904fd0ce5
Manuel Reich <mr@wundf.net>
2019-05-01 13:42:34 +0200

    Style clients and topStory

--- components/Clients/Clients.index.js
+++ components/Clients/Clients.index.js
@@ -4,19 +4,47 @@ import { getAbsolutAssetURL } from '../../lib/apiHelpers'
 import { lightGrey } from '../../lib/colors'
 
 const Wrapper = styled.section`
-  background-color: ${lightGrey}
+  background-color: ${lightGrey};
+  padding: 25px 20px 15px;
+  margin-bottom: 20px;
+`
+
+const Title = styled.h1`
+  font-style: italic;
+  font-weight: normal;
+  font-size: 15px;
+  margin: 0 0 20px;
+`
+
+const Logos = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: baseline;
+  justify-content: space-between;
 `
 
 const Logo = styled.img`
+  height: ${props => 20 * props.relativeSize}px;
+  margin: 0 15px 20px 0;
+`
 
+const Spacer = styled.div`
+  flex: 0 0 40%;
 `
 
 const Clients = ({ clients }) => {
-  console.log(clients)
   return <Wrapper>
-    {clients.map((client, i) =>
-      <Logo key={i} src={getAbsolutAssetURL(client.logo.path)} alt={client.name} />
-    )}
+    <Title>Kunden</Title>
+    <Logos>
+      {clients.map((client, i) =>
+        <Logo
+          key={i}
+          src={getAbsolutAssetURL(client.logo.path)}
+          relativeSize={client.relativeSize}
+          alt={client.name} />
+      )}
+      <Spacer />
+    </Logos>
   </Wrapper>
 }
 
--- components/WorkOverview/TopStory.js
+++ components/WorkOverview/TopStory.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getAbsolutAssetURL } from '../../lib/apiHelpers'
 import Attribution from './Attribution'
+import { css } from '@emotion/core'
 import styled from '@emotion/styled'
 import colors from '../../lib/colors'
 import breakpoints from '../../lib/breakpoints'
@@ -31,28 +32,41 @@ const Image = styled.figure`
     position: absolute;
     width: 100%;
     height: 100%;
-    background: linear-gradient(to right, rgba(0,0,0, 0.8) 10%, rgba(0,0,0, 0.5) 50%, rgba(0,0,0, 0.1) 80%, rgba(0,0,0, 0));
+    background: linear-gradient(to right, rgba(0,0,0, 0.7), rgba(0,0,0, 0));
   }
 `
 
-const Title = styled.h1`
+const whiteText = css`
   color: white;
-  font-size: 30px;
-  font-weight: 400;
-  margin: 0 0 50px;
+  text-shadow: 0 0 2px black;
+  line-height: 1.2em;
   -webkit-font-smoothing: grayscale;
   -moz-osx-font-smoothing: grayscale;
   -o-font-smoothing: grayscale;
 `
 
+const Title = styled.h1`
+  ${whiteText};
+  text-shadow: none;
+  font-size: 30px;
+  font-weight: 400;
+  margin: 0 0 50px;
+`
+
 const Teaser = styled.p`
+  ${whiteText};
   width: 80%;
   color: white;
-  margin: 0;
-  text-shadow: 0 0 4px black;
-  -webkit-font-smoothing: grayscale;
-  -moz-osx-font-smoothing: grayscale;
-  -o-font-smoothing: grayscale;
+  margin: 0 0 20px;
+`
+
+const Link = styled.a`
+  ${whiteText};
+  font-weight: bold;
+
+  &::before {
+    content: '+ ';
+  }
 `
 
 const TopStory = ({ story }) =>
@@ -63,6 +77,7 @@ const TopStory = ({ story }) =>
 
     <Title>{story.title}</Title>
     <Teaser>{story.teaserText}</Teaser>
+    <Link>{story.linkText}</Link>
   </Wrapper>
 
 export default TopStory
