1492d89c6e1e1241a23693fd006132182f12533a
Manuel Reich <mr@wundf.net>
2021-03-08 19:31:01 +0100

    Hardcode backend url

--- lib/apiHelpers.ts
+++ lib/apiHelpers.ts
@@ -9,7 +9,7 @@ export async function fetchFromBackend<T>(
   validator: Validator<T>
 ): Promise<FetchResult<T>> {
   try {
-    const res = await fetch(`${process.env.BACKEND_URL}${name}`)
+    const res = await fetch(`https://backend.wunderundfitzig.de${name}`)
     const data = await res.json()
     const validData = validator(data)
     return success(validData)
