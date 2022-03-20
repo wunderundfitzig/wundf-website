cba05c15faa072d97c40af84e6a467e6e6759d53
Manuel Reich <mr@wundf.net>
2022-03-20 19:06:16 +0100

    Allow to install user via panel

--- backend/site/config/config.php
+++ backend/site/config/config.php
@@ -5,5 +5,8 @@ header('Access-Control-Allow-Headers: content-type, authorization, origin, x-req
 header('Access-Control-Allow-Methods: GET');
 
 return [
-  "debug" => true
+  "debug" => true,
+  'panel' =>[
+    'install' => true
+  ]
 ];
