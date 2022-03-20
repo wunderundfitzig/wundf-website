3bff44835a5735928ec161e858893cb1bf142f2f
Manuel Reich <mr@wundf.net>
2022-03-20 18:32:52 +0100

    Remove webhoks plugin
    we don't need it anymore since we use automatic regeneration
    on the frontend

--- backend/site/config/config.php
+++ backend/site/config/config.php
@@ -5,13 +5,5 @@ header('Access-Control-Allow-Headers: content-type, authorization, origin, x-req
 header('Access-Control-Allow-Methods: GET');
 
 return [
-  "errnesto.webhooks.getHeader" => function($trigger, $webhook, ...$params) {
-    $auth = base64_encode($webhook['username'] . ":" . $webhook['password']);
-    return "Authorization: Basic $auth\r\n" .
-           "Content-type: application/json\r\n";
-  },
-  "errnesto.webhooks.getPayload" => function($trigger, $webhook, ...$params) {
-    return "";
-  },
   "debug" => false
 ];
