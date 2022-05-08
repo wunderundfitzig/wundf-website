2a9ead847290fd212dabb19f867ebe754a9101ab
Manuel Reich <mr@wundf.net>
2022-05-08 16:57:56 +0200

    Dissable debug settings in panel

--- backend/site/config/config.php
+++ backend/site/config/config.php
@@ -5,8 +5,8 @@ header('Access-Control-Allow-Headers: content-type, authorization, origin, x-req
 header('Access-Control-Allow-Methods: GET');
 
 return [
-  "debug" => true,
-  'panel' =>[
-    'install' => true
-  ]
+  // "debug" => true,
+  // 'panel' =>[
+  //   'install' => true
+  // ]
 ];
