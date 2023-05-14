d6e473931a88d283361b38f1039a44e7285e395d
Manuel Reich <mr@wundf.net>
2023-05-14 13:23:29 +0200

    Try to add image dimensions to block images

--- backend/site/plugins/headless-blocks/index.php
+++ backend/site/plugins/headless-blocks/index.php
@@ -9,8 +9,12 @@ Kirby::plugin('errnesto/headless-blocks', [
             if (class_uses($model, 'Kirby\Cms\HasFiles')) {
                 array_walk_recursive($blocks, function (&$value, $key) use ($model) {
                     if (is_int($key) && is_string($value)) {
-                        if($file = $model->file($value)) {
-                            $value = $file->id();
+                        if ($file = $model->file($value)) {
+                            $value = [
+                                "src" => $file->id(),
+                                "width" => 0,
+                                "height" => 0
+                            ];
                         }
                     }
                 });
