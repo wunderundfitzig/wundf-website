77a90487c1a8d5eec3585a4ec5f0429457364993
Manuel Reich <mr@wundf.net>
2023-05-29 00:00:55 +0200

    Back to latest next

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,13 +8,12 @@ module.exports = withBundleAnalyzer({
     backendURL: process.env.BACKEND_URL,
   },
   images: {
-    // unoptimized: true,
+    unoptimized: true,
     domains: ['cms.wunderundfitzig.de'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
   experimental: {
     scrollRestoration: true,
-    appDir: true,
   },
   async redirects() {
     return [
