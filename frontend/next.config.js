e672e77254396f665af05aa0926e483a7c3445b6
Manuel Reich <mr@wundf.net>
2023-05-28 23:38:23 +0200

    Optimize images but set smaller devices sizes list

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,8 +8,9 @@ module.exports = withBundleAnalyzer({
     backendURL: process.env.BACKEND_URL,
   },
   images: {
-    unoptimized: true,
+    // unoptimized: true,
     domains: ['cms.wunderundfitzig.de'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
   experimental: {
     scrollRestoration: true,
